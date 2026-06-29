9acb322244f6cb71504cd5a673a7fa0a09c8e29a
trungvuong3xx <trungvuong3xx@gmail.com>
2026-06-29 07:03:22 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,15 @@
 const CACHE = "kho-v1";
-const FILES = ["/", "/index.html", "/app.js", "/style.css", "/cx1.js", "/kiemke.js", "/manifest.json", "/icon-192.png", "/icon-512.png"];
-
+const FILES = [
+  "/Quan-ly-kho/",
+  "/Quan-ly-kho/index.html",
+  "/Quan-ly-kho/app.js",
+  "/Quan-ly-kho/style.css",
+  "/Quan-ly-kho/cx1.js",
+  "/Quan-ly-kho/kiemke.js",
+  "/Quan-ly-kho/manifest.json",
+  "/Quan-ly-kho/icon-192.png",
+  "/Quan-ly-kho/icon-512.png"
+];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
   self.skipWaiting();
